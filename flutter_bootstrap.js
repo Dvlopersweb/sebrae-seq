df40e2b1a2b5385cb50b6a18d7f18896fca8aa59
Dvlopers <dvlopers.web@gmail.com>
2024-10-07 21:46:11 -0300

    editada

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "680027401"
+    serviceWorkerVersion: "614090518"
   }
 });
